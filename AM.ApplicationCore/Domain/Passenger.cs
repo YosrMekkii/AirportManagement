bd6529a28a06f31264ebd59561f7283c887a6a98
YosrMekkii <yosr.mekki@esprit.tn>
2025-02-24 10:01:00 +0100

    Ajoutez des fichiers projet.

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Key]
+        public string PassportNumber { get; set; }
+        
+        public string TelNumber { get; set; }
+        public IList<Flight> Flights { get; set; }
+        public virtual string PassengerType { get { return "Unknown passenger type"; } }
+        public override string ToString()
+        {
+            return $"PassportNumber : {PassportNumber}, FirstName : {FirstName}, LastName : {LastName}";
+        }
+    }
+}
